59d68c4a7118504ace62f31b3500fc6baf29f68a
kingwell47 <joeldoctor47@gmail.com>
2021-10-21 17:11:53 +0800

    added routes and model files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +9,37 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
+
+//Connect to DB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+//Solution Endpoint
+app.post("/api/shorturl", function (req, res) {
+  res.json({ original_url: "original", short_url: "short" });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
